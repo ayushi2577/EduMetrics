d2da6ede8ac9e664f0395a5e71c6820696118102
Suhani Sharma <suhanisharma150708@gmail.com>
2026-05-02 14:25:17 +0530

    added actual semester (CSE_Y2 semester2 means BTECH sem 4)

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -30,13 +30,26 @@ def login(request):
         if password==actual:
             # Get latest semester and week for this advisor's class
             from analysis_engine.models import analysis_state as AnalysisState
+            from accounts.models import Users as Advisor
+            classXsem={
+                    'CSE_Y1_A':1,
+                    'CSE_Y2_A':3,
+                    'CSE_Y3_A':5,
+                    'CSE_Y4_A':7
+                }
             try:
                 state = AnalysisState.objects.get(id=1)
                 max_sem  = state.current_semester
                 max_week = state.current_sem_week
+                class_id = advisor.class_id
+                if max_sem==1:
+                    actual_semester = classXsem.get(class_id)
+                else:
+                    actual_semester = classXsem.get(class_id)+1
             except AnalysisState.DoesNotExist:
                 max_sem  = 0
                 max_week = 0
+                actual_semester = classXsem.get(class_id)
 
             res = {
                 'message':      'Login successful',
@@ -45,6 +58,7 @@ def login(request):
                 'advisor_name': advisor.advisor_name,
                 'semester':     max_sem,
                 'sem_week':     max_week,
+                'actual_semester': actual_semester
             }
             tokens = get_token(advisor)
             res.update(tokens)
