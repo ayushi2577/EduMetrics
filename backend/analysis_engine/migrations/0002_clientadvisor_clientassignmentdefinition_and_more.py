64e589d25d6e97a7d1ba6c63a1600d917186bbf9
ayushi2577 <ayushi.agrawal_cs.h25@gla.ac.in>
2026-06-06 17:17:34 +0530

    fixing migration dependencies

--- backend/analysis_engine/migrations/0002_clientadvisor_clientassignmentdefinition_and_more.py
+++ backend/analysis_engine/migrations/0002_clientadvisor_clientassignmentdefinition_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('analysis_engine', '001_initial'),
+        ('analysis_engine', '0001_initial'),
     ]
 
     operations = [
