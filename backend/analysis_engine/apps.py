6cbfa778e32b2c7d9bd666d9dd920b748fd2fde2
ayushi2577 <ayushi.agrawal_cs.h25@gla.ac.in>
2026-06-06 21:12:34 +0530

    syncing

--- backend/analysis_engine/apps.py
+++ backend/analysis_engine/apps.py
@@ -4,4 +4,14 @@ class AnalysisEngineConfig(AppConfig):
     name = 'analysis_engine'
 
     def ready(self):
-        pass
+        try:
+            from .calibrate_analysis_db import calibrate
+            calibrate()
+        except Exception as e:
+            print(f"calibrate error: {e}")
+        
+        try:
+            from accounts.addingdata import sync
+            sync()
+        except Exception as e:
+            print(f"sync error: {e}")
