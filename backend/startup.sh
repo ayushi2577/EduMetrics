836c1b1a08eecef23f3cdb83e14abd2c64b44447
ayushi2577 <ayushi.agrawal_cs.h25@gla.ac.in>
2026-06-06 20:48:36 +0530

    fake initial migration

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-# 1. Apply all migrations for real (no faking)
-python manage.py migrate --noinput
+# #!/bin/bash
+# # 1. Apply all migrations for real (no faking)
+# python manage.py migrate --noinput
 
-# 2. Collect static files
+# # 2. Collect static files
+# python manage.py collectstatic --noinput
+
+# # 3. Start the server
+# gunicorn config.wsgi:application --bind 0.0.0.0:8000 --timeout 120
+# #!/bin/bash
+python manage.py migrate --fake-initial --noinput
 python manage.py collectstatic --noinput
-
-# 3. Start the server
-gunicorn config.wsgi:application --bind 0.0.0.0:8000 --timeout 120
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
